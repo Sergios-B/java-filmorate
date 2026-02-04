4580ba936e2ee022be5f1a15afed9eb106b4f1fb
Barkov <venediktkaverin@gmail.com>
2026-02-04 07:54:46 +0300

    исправить класс UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
@@ -16,10 +17,12 @@ import java.util.Collection;
 @RequestMapping("/users")
 public class UserController {
     private final UserStorage userStorage;
+    private final UserService userService; // Внедряем сервис
 
     @Autowired
-    public UserController(UserStorage userStorage) {
+    public UserController(UserStorage userStorage, UserService userService) {
         this.userStorage = userStorage;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -58,54 +61,41 @@ public class UserController {
 
     @PutMapping("/{id}/friends/{friendId}")
     public void addFriend(@PathVariable Long id, @PathVariable Long friendId) {
-        if (!userStorage.findUserById(id) || !userStorage.findUserById(friendId)) {
-            throw new NotFoundException("Пользователь или друг не найден");
-        }
-        userStorage.addFriend(id, friendId);
+        log.info("Пользователь {} добавляет в друзья {}", id, friendId);
+        userService.addFriend(id, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
     public void removeFriend(@PathVariable Long id, @PathVariable Long friendId) {
-        if (!userStorage.findUserById(id) || !userStorage.findUserById(friendId)) {
-            throw new NotFoundException("Пользователь или друг не найден");
-        }
-        userStorage.removeFriend(id, friendId);
+        log.info("Пользователь {} удаляет из друзей {}", id, friendId);
+        userService.removeFriend(id, friendId);
     }
 
     @GetMapping("/{id}/friends")
     public Collection<User> getFriends(@PathVariable Long id) {
-        if (!userStorage.findUserById(id)) {
-            throw new NotFoundException("Пользователь не найден");
-        }
-        return userStorage.getMyFriends(id);
+        log.info("Запрос списка друзей пользователя {}", id);
+        return userService.getMyFriends(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
     public Collection<User> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
-        if (!userStorage.findUserById(id) || !userStorage.findUserById(otherId)) {
-            throw new NotFoundException("Один из пользователей не найден");
-        }
-        return userStorage.getCommonFriends(id, otherId);
+        log.info("Запрос общих друзей пользователей {} и {}", id, otherId);
+        return userService.getCommonFriends(id, otherId);
     }
 
     private void validate(User user) {
         if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
-            log.warn("Валидация не пройдена: некорректный email '{}'", user.getEmail());
-            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
+            throw new ValidationException("Электронная почта некорректна");
         }
         if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
-            log.warn("Валидация не пройдена: некорректный логин '{}'", user.getLogin());
-            throw new ValidationException("Логин не может быть пустым или содержать пробелы");
+            throw new ValidationException("Логин некорректен");
         }
         if (user.getName() == null || user.getName().isBlank()) {
-            log.info("Имя пользователя пустое, в качестве имени будет использован логин: {}", user.getLogin());
             user.setName(user.getLogin());
         }
         if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
-            log.warn("Валидация не пройдена: дата рождения {} в будущем", user.getBirthday());
-            throw new ValidationException("Дата рождения не может быть в будущем");
+            throw new ValidationException("Дата рождения в будущем");
         }
-        log.info("Валидация пользователя '{}' пройдена успешно.", user.getLogin());
     }
 
     private long getNextId() {
