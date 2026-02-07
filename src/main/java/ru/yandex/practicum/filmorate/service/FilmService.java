e58b822655178b1b9b052fc898885db6de731e7b
Barkov <venediktkaverin@gmail.com>
2026-02-07 12:24:05 +0300

    Исправить валидацию

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,21 +17,23 @@ import java.util.stream.Collectors;
 @Service
 public class FilmService {
     private final FilmStorage filmStorage;
-    private final UserStorage userStorage;
+    private final UserService userService;
+    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
 
     @Autowired
-    public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
+    public FilmService(FilmStorage filmStorage, UserService userService) {
         this.filmStorage = filmStorage;
-        this.userStorage = userStorage;
+        this.userService = userService;
     }
 
-
     public Film create(Film film) {
+        validateReleaseDate(film);
         log.info("Добавление нового фильма: {}", film.getName());
         return filmStorage.add(film);
     }
 
     public Film update(Film film) {
+        validateReleaseDate(film);
         log.info("Обновление фильма с id: {}", film.getId());
         return filmStorage.update(film);
     }
@@ -45,23 +49,16 @@ public class FilmService {
                 .orElseThrow(() -> new NotFoundException("Фильм с id " + id + " не найден"));
     }
 
-
     public void addLike(Integer filmId, Integer userId) {
         Film film = findById(filmId);
-        if (userStorage.findById(userId).isEmpty()) {
-            throw new NotFoundException("Пользователь с id " + userId + " не найден");
-        }
-
+        userService.findById(userId);
         film.getLikes().add(userId);
         log.info("Пользователь {} поставил лайк фильму {}", userId, filmId);
     }
 
     public void removeLike(Integer filmId, Integer userId) {
         Film film = findById(filmId);
-        if (userStorage.findById(userId).isEmpty()) {
-            throw new NotFoundException("Пользователь с id " + userId + " не найден");
-        }
-
+        userService.findById(userId);
         film.getLikes().remove(userId);
         log.info("Пользователь {} удалил лайк у фильма {}", userId, filmId);
     }
@@ -69,8 +66,15 @@ public class FilmService {
     public List<Film> getPopularFilms(int count) {
         log.info("Запрос на получение {} самых популярных фильмов", count);
         return filmStorage.findAll().stream()
-                .sorted((f1, f2) -> f2.getLikes().size() - f1.getLikes().size()) // Сортировка по убыванию
+                .sorted(Comparator.comparingInt((Film f) -> f.getLikes().size()).reversed())
                 .limit(count)
                 .collect(Collectors.toList());
+    }
+
+    private void validateReleaseDate(Film film) {
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
+            log.error("Ошибка валидации даты релиза: {}", film.getReleaseDate());
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
     }
 }
